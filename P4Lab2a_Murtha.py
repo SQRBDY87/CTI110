431b1786111a8f4f16563a06669638602966dfaa
Josh Murtha <107258307+SQRBDY87@users.noreply.github.com>
2022-06-30 18:57:20 -0400

    Add files via upload

---
+++ P4Lab2a_Murtha.py
@@ -0,0 +1,35 @@
+#CTI-110
+#P4LAB2-Turtle Lab
+#Joshua Murtha
+#6/28/2022
+
+#Square
+import turtle
+turtle.showturtle()
+turtle.shape("turtle")
+turtle.pencolor('black')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+josh=turtle.Turtle()
+length=100
+angle=90
+
+
+#Triangle
+length=100
+angel=120
+for i in range(3):
+    turtle.pencolor("red")
+    turtle.forward(100)
+    turtle.right(120)
+turtle.end_fill()
+
+
+
+
+
---
+++ P4Lab2b_Murtha.py
@@ -0,0 +1,39 @@
+#CTI-110
+#P4LAB-Turtle Graphic
+#Joshua Murtha
+#6/29/2022
+
+import turtle as t
+t.penup()
+
+t.goto(-60,50)
+t.pendown()
+t.pensize(10)
+t.pencolor("blue")
+
+t.forward(10)
+t.right(90)
+t.forward(120)
+t.circle(-40,180)
+t.penup()
+
+t.goto(-30,50)
+t.pendown()
+t.right(180)
+t.forward(150)
+
+t.goto(-30,50)
+t.goto(20,-20)
+t.goto(65,50)
+t.goto(65,-100)
+t.penup()
+
+
+
+
+
+
+
+
+
+
